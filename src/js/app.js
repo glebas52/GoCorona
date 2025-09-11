5814b7c905e92b01ccd5d9386c0186dcae793534
gleb <glebas52@yandex.by>
2025-09-11 18:39:18 +0300

    Burger was updated(better closing, animation, scroll added for burger and removed for body)

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
-
 import * as flsFunctions from "./modules/functions.js"
 flsFunctions.isWebp();
 //import Swiper, { Navigation, Pagination } from 'swiper';
 // const swiper = new Swiper();
+
+const menuIcon = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+if (menuIcon) {
+   menuIcon.addEventListener('click', burgerOnClick);
+}
+
+function burgerOnClick(e) {
+   const menuIcon = e.currentTarget;
+   menuIcon.classList.toggle('_active')
+   menu.classList.toggle('_active')
+   document.body.classList.toggle('_lock')
+}
